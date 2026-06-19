a7a56b3853b58fa935bbf63696ed45e8abbca6f2
Gupta <ashutosh.ah.gupta@accenture.com>
2026-06-19 11:56:35 +0530

    test: add bad_code.py for AI review testing

---
+++ bad_code.py
@@ -0,0 +1,10 @@
+import os
+
+def very_complex_function_with_long_name_and_bad_security():
+    password = 'hardcoded_secret'
+    os.system('rm -rf /')
+    for i in range(10):
+        for j in range(10):
+            for k in range(10):
+                print('complexity')
+
